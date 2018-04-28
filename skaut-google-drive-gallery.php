03017bfcff786e816ae0e52f1075b2afdcf22a55
GenaBitu <genabitu@gmail.com>
2018-04-28 16:18:50 +0200

    Added settings boilerplate

--- skaut-google-drive-gallery.php
+++ skaut-google-drive-gallery.php
@@ -31,3 +31,71 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+defined('ABSPATH') or die('Die, die, die!');
+
+if(!class_exists('Sgdg_plugin'))
+{
+	class Sgdg_plugin
+	{
+		public static function init() : void
+		{
+			add_action('admin_init', ['Sgdg_plugin', 'plugin_register_settings']);
+			add_action('admin_menu', ['Sgdg_plugin', 'plugin_options_page']);
+		}
+
+		public static function plugin_options_page() : void
+		{
+			add_options_page('Google drive gallery', 'Google drive gallery', 'manage_options', 'sgdg', ['Sgdg_plugin', 'options_page_html']);
+		}
+
+		public static function plugin_register_settings() : void
+		{
+			register_setting('sgdg', 'sgdg_setting_name', ['type' => 'string']);
+			add_settings_section('sgdg_main_section', 'Hlavní nastavení', ['Sgdg_plugin', 'main_section_top'], 'sgdg');
+			add_settings_field('sgdg_setting1', '1. vlastnost', ['Sgdg_plugin', 'setting1_top'], 'sgdg', 'sgdg_main_section');
+		}
+
+		function options_page_html() : void
+		{
+			if (!current_user_can('manage_options'))
+			{
+				return;
+			}
+ 
+			if(isset($_GET['settings-updated']))
+			{
+			add_settings_error('sgdg_messages', 'sgdg_message', 'Settings Saved', 'updated');
+			}
+ 
+			settings_errors('sgdg_messages');
+			?>
+			<div class="wrap">
+				<h1><?php echo(esc_html(get_admin_page_title())); ?></h1>
+				<form action="options.php" method="post">
+					<?php
+					settings_fields('sgdg');
+					do_settings_sections('sgdg');
+					submit_button('Save Settings');
+					?>
+				</form>
+			</div>
+			<?php
+		}
+
+		function main_section_top() : void
+		{
+			echo('<p>Hlavní nastavení</p>');
+		}
+
+		function setting1_top() : void
+		{
+			$setting = get_option('sgdg_setting_name');
+			?>
+			<input type="text" name="sgdg_setting_name" value="<?php echo(isset($setting) ? esc_attr($setting) : ''); ?>">
+			<?php
+		}
+	}
+
+	Sgdg_plugin::init();
+}
