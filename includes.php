a08bf251e6751d2e2495e92e63bec51fe75a5f47
GenaBitu <genabitu@gmail.com>
2018-05-08 13:48:06 +0200

    Root dir selection works with minimised Google API library

--- includes.php
+++ includes.php
@@ -6,6 +6,11 @@ include_once('vendor/psr/http-message/src/RequestInterface.php');
 include_once('vendor/psr/http-message/src/StreamInterface.php');
 include_once('vendor/psr/http-message/src/ResponseInterface.php');
 
+include_once('vendor/psr/log/Psr/Log/LoggerInterface.php');
+
+include_once('vendor/psr/cache/src/CacheItemPoolInterface.php');
+include_once('vendor/psr/cache/src/CacheItemInterface.php');
+
 include_once('vendor/guzzlehttp/guzzle/src/Handler/StreamHandler.php');
 include_once('vendor/guzzlehttp/guzzle/src/Handler/CurlHandler.php');
 include_once('vendor/guzzlehttp/guzzle/src/Handler/CurlFactoryInterface.php');
@@ -38,15 +43,54 @@ include_once('vendor/guzzlehttp/promises/src/TaskQueueInterface.php');
 include_once('vendor/guzzlehttp/promises/src/TaskQueue.php');
 include_once('vendor/guzzlehttp/promises/src/FulfilledPromise.php');
 
+include_once('vendor/monolog/monolog/src/Monolog/Handler/HandlerInterface.php');
+include_once('vendor/monolog/monolog/src/Monolog/Handler/AbstractHandler.php');
+include_once('vendor/monolog/monolog/src/Monolog/Handler/AbstractProcessingHandler.php');
+include_once('vendor/monolog/monolog/src/Monolog/Handler/StreamHandler.php');
+
+include_once('vendor/monolog/monolog/src/Monolog/Logger.php');
+
+include_once('vendor/google/auth/src/Cache/MemoryCacheItemPool.php');
+include_once('vendor/google/auth/src/Cache/Item.php');
+
 include_once('vendor/google/auth/src/HttpHandler/Guzzle6HttpHandler.php');
 include_once('vendor/google/auth/src/HttpHandler/HttpHandlerFactory.php');
 
+include_once('vendor/google/auth/src/CacheTrait.php');
 include_once('vendor/google/auth/src/FetchAuthTokenInterface.php');
 include_once('vendor/google/auth/src/OAuth2.php');
 
+include_once('vendor/google/auth/src/Middleware/ScopedAccessTokenMiddleware.php');
+
+include_once('vendor/google/apiclient/src/Google/Http/REST.php');
+
+include_once('vendor/google/apiclient/src/Google/Task/Runner.php');
+
 include_once('vendor/google/apiclient/src/Google/AccessToken/Revoke.php');
 
+include_once('vendor/google/apiclient/src/Google/AuthHandler/AuthHandlerFactory.php');
+include_once('vendor/google/apiclient/src/Google/AuthHandler/Guzzle6AuthHandler.php');
+
+include_once('vendor/google/apiclient/src/Google/Service/Resource.php');
+
+include_once('vendor/google/apiclient/src/Google/Utils/UriTemplate.php');
+
 include_once('vendor/google/apiclient/src/Google/Client.php');
+include_once('vendor/google/apiclient/src/Google/Model.php');
+include_once('vendor/google/apiclient/src/Google/Collection.php');
 include_once('vendor/google/apiclient/src/Google/Service.php');
+
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/DriveFile.php');
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/FileList.php');
+
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/Resource/About.php');
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/Resource/Changes.php');
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/Resource/Channels.php');
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/Resource/Comments.php');
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/Resource/Files.php');
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/Resource/Permissions.php');
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/Resource/Replies.php');
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/Resource/Revisions.php');
+include_once('vendor/google/apiclient-services/src/Google/Service/Drive/Resource/Teamdrives.php');
 
 include_once('vendor/google/apiclient-services/src/Google/Service/Drive.php');
