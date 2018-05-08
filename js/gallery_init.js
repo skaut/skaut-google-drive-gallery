4ce34455d3ce18f6472c1facae81b5492e1f4427
GenaBitu <genabitu@gmail.com>
2018-05-08 15:02:55 +0200

    Removed masonry horizontalOrder

--- js/gallery_init.js
+++ js/gallery_init.js
@@ -3,7 +3,6 @@ jQuery(document).ready(function($) {
 	var grid = $('.grid').masonry({
 		itemSelector: '.grid-item',
 		columnWidth: 250,
-		horizontalOrder: true,
 		gutter: 10,
 		fitWidth: true
 	});
