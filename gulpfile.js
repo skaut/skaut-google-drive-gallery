1fca620d062ca52cfb2c5d16828cb3940b814dd1
GenaBitu <genabitu@gmail.com>
2018-05-14 15:45:37 +0200

    npm dependency updating

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,32 @@ gulp.task("composer-check-updates", function(done)
 
 gulp.task("npm-check-updates", function(done)
 	{
-		return npmcheck({"skipUnused": true, "ignore": ["jquery"]}, done);
+		npmcheck({"skipUnused": true, "ignore": ["jquery"]}, done);
 	});
+
+function npmDoUpdate(done)
+{
+	npmcheck({"skipUnused": true, "ignore": ["jquery"], "update": true, "ignoreDev": true}, done);
+}
+
+function copyImagelightbox()
+{
+	return gulp.src("node_modules/imagelightbox/dist/imagelightbox.min.*")
+		.pipe(gulp.dest("plugin/bundled/"));
+}
+
+function copyImagesloaded()
+{
+	return gulp.src("node_modules/imagesloaded/imagesloaded.pkgd.min.js")
+		.pipe(gulp.dest("plugin/bundled/"));
+}
+
+function copyMasonry()
+{
+	return gulp.src("node_modules/masonry-layout/dist/masonry.pkgd.min.js")
+		.pipe(gulp.dest("plugin/bundled/"));
+}
+
+gulp.task("npm-update", gulp.series(npmDoUpdate, gulp.parallel(copyImagelightbox, copyImagesloaded, copyMasonry)));
 
 gulp.task("default", gulp.series("composer-check-updates", "npm-check-updates"));
