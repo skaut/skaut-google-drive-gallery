2bc9c6b746e8e121ce07acb96a9b61e1400fd7a3
GenaBitu <genabitu@gmail.com>
2018-05-14 15:00:56 +0200

    composer deps checking

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 const gulp = require("gulp");
 const npmcheck = require("gulp-npm-check");
+const composer = require("gulp-composer");
 
+gulp.task("composer-check-updates", function(done)
+	{
+		composer("show -l", {"self-install": false, "async": false})
+		done();
+	});
 
 gulp.task("npm-check-updates", function(done)
 	{
 		return npmcheck({"skipUnused": true, "ignore": ["jquery"]}, done);
 	});
 
-gulp.task("default", gulp.series("npm-check-updates"));
+gulp.task("default", gulp.series("composer-check-updates", "npm-check-updates"));
