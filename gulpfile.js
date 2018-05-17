e0df2c93727b1164081dcc79ca4145208bb3a738
GenaBitu <genabitu@gmail.com>
2018-05-17 11:15:58 +0200

    Removed gulp-wp-pot

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require("gulp");
 const npmcheck = require("gulp-npm-check");
 const composer = require("gulp-composer");
 const shell = require("gulp-shell");
-const wpPot = require('gulp-wp-pot');
 
 gulp.task("composer-check-updates", function(done)
 	{
@@ -41,19 +40,5 @@ function copyMasonry()
 gulp.task("npm-update", gulp.series(npmDoUpdate, gulp.parallel(copyImagelightbox, copyImagesloaded, copyMasonry)));
 
 gulp.task("phpcs", shell.task(["vendor/squizlabs/php_codesniffer/bin/phpcs"]));
-
-gulp.task("pot-update", function(done)
-	{
-		return gulp.src("plugin/**/*.php", "!plugin/bundled/**")
-			.pipe(wpPot({
-				bugReport: "https://wordpress.org/support/plugin/skaut-google-drive-gallery",
-				headers: false,
-				lastTranslator: "FULL NAME <EMAIL@ADDRESS>",
-				metadataFile: "skaut-google-drive-gallery.php",
-				package: "Google drive gallery",
-				relativeTo: "plugin/"
-				}))
-			.pipe(gulp.dest('plugin/languages/skaut-google-drive-gallery.pot'));
-	});
 
 gulp.task("default", gulp.series("composer-check-updates", "npm-check-updates"));
