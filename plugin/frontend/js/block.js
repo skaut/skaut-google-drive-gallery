5ae187b23774042cc905bb67c22f1515c8d53e92
GenaBitu <genabitu@gmail.com>
2018-05-28 22:29:41 +0200

    Semi-working editor

--- plugin/frontend/js/block.js
+++ plugin/frontend/js/block.js
@@ -10,20 +10,79 @@ jQuery( document ).ready(function($) {
 		icon: "format-gallery",
 		attributes: {
 			path: {
-				type: "array"
+				type: "array",
+				default: []
 			}
 		},
 		edit: render_editor,
-		save: render_frontend
+		save: render_frontend,
+		useOnce: true // TODO: Remove
 	});
 
-	function render_editor(attributes)
+	function render_editor(props)
 	{
-		return el( "p", { style: blockStyle }, "Hello editor content." );
+		ajax_query(props);
+		return el( "table", { class: "widefat" }, [
+			el("thead", {},
+				el("tr", {},
+					el("th", {class: "sgdg-block-editor-path"}, "ROOT") // TODO: i18n
+				)
+			),
+			el("tbody", {id: "sgdg-block-editor-list"}),
+			el("tfoot", {},
+				el("tr", {},
+					el("th", {class: "sgdg-block-editor-path"}, "ROOT") // TODO: i18n
+				)
+			)
+		]);
 	}
 
-	function render_frontend(attributes)
+	function render_frontend(props)
 	{
 		return el( "p", { style: blockStyle }, "Hello saved content." );
+	}
+
+	function ajax_query(props)
+	{
+		var path = props.attributes.path;
+		$( "#sgdg-block-editor-list" ).html( "" );
+		$.get(sgdg_block_localize.ajax_url, {
+			_ajax_nonce: sgdg_block_localize.nonce,
+			action: "list_gallery_dir",
+			path: path
+			}, function(data)
+			{
+				var html = "";
+				if (path.length > 0) {
+					html += "<tr><td class=\"row-title\"><label>..</label></td></tr>";
+				}
+				var len = data.length;
+				for (var i = 0; i < len; i++) {
+					html += "<tr class=\"";
+					if ((path.length === 0 && i % 2 === 1) || (path.length > 0 && i % 2 === 0)) {
+						html += "alternate";
+					}
+					html += "\"><td class=\"row-title\"><label>" + data[i] + "</label></td></tr>";
+				}
+				$( "#sgdg-block-editor-list" ).html( html );
+				html = sgdg_block_localize.root_name;
+				len  = path.length;
+				for (i = 0; i < len; i++) {
+					html += " > ";
+					html += path[i];
+				}
+				$( ".sgdg-block-editor-path" ).html( html );
+				$( "#sgdg-block-editor-list label" ).click(function() {
+					var newDir = $( this ).html();
+					if (newDir === "..") {
+						path.pop();
+					} else {
+						path.push( newDir );
+					}
+					props.setAttributes({path: path})
+					ajax_query(props);
+				});
+			}
+		);
 	}
 });
