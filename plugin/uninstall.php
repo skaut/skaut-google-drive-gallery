32b9c32520f678f37f6f9b6c65ac64793fb655f5
GenaBitu <genabitu@gmail.com>
2018-06-07 19:09:32 +0200

    Updated uninstall hook

--- plugin/uninstall.php
+++ plugin/uninstall.php
@@ -6,11 +6,26 @@ delete_option( 'sgdg_client_id' );
 delete_option( 'sgdg_client_secret' );
 delete_option( 'sgdg_access_token' );
 delete_option( 'sgdg_root_path' );
-delete_option( 'sgdg_thumbnail_size' );
-delete_option( 'sgdg_thumbnail_spacing' );
 delete_option( 'sgdg_preview_size' );
 delete_option( 'sgdg_preview_speed' );
 delete_option( 'sgdg_preview_arrows' );
 delete_option( 'sgdg_preview_closebutton' );
 delete_option( 'sgdg_preview_loop' );
 delete_option( 'sgdg_preview_activity' );
+delete_option( 'sgdg_dir_counts' );
+delete_option( 'sgdg_image_ordering_order' );
+delete_option( 'sgdg_image_ordering_by' );
+delete_option( 'sgdg_date_ordering_order' );
+delete_option( 'sgdg_date_ordering_by' );
+delete_option( 'sgdg_grid_mode' );
+delete_option( 'sgdg_grid_width' );
+delete_option( 'sgdg_grid_columns' );
+delete_option( 'sgdg_grid_min_width' );
+delete_option( 'sgdg_grid_spacing' );
+
+// Deprecated
+delete_option( 'sgdg_image_ordering' );
+delete_option( 'sgdg_thumbnail_size' );
+delete_option( 'sgdg_thumbnail_size_value' );
+delete_option( 'sgdg_thumbnail_size_unit' );
+delete_option( 'sgdg_thumbnail_spacing' );
