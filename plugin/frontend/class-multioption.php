ed6f9dd7448b669cb3bdf228c905655fa66840a0
GenaBitu <genabitu@gmail.com>
2018-06-07 15:29:46 +0200

    MultiOption sub options

--- plugin/frontend/class-multioption.php
+++ plugin/frontend/class-multioption.php
@@ -1,22 +1,30 @@
 <?php
 namespace Sgdg\Frontend;
 
-
 class MultiOption extends Option {
 	private $values;
+	private $subOptions;
 
 	public function __construct( $name, $values, $default_value, $section, $title ) {
-		if ( !array_key_exists( $default_value, $values ) );
-		{
-			if ( count( $values ) > 0 )
-			{
-				$default_value = $values[0];
+		if ( !array_key_exists( $default_value, $values ) ) {
+			if ( count( $values ) > 0 ) {
+				reset($values);
+				$default_value = key($values);
 			} else {
 				$default_value = '';
 			}
 		}
 		parent::__construct( $name, $default_value, $section, $title );
 		$this->values = $values;
+		$this->subOptions = [];
+		foreach($values as $value => $_)
+		{
+			$this->subOptions[$value] = [];
+		}
+	}
+
+	public function addSubOption($value, \Sgdg\Frontend\Option $option) {
+		$this->subOptions[$value][] = $option;
 	}
 
 	public function register() {
@@ -24,6 +32,13 @@ class MultiOption extends Option {
 			'type'              => 'string',
 			'sanitize_callback' => [ $this, 'sanitize' ],
 		]);
+		foreach($this->values as $value => $_)
+		{
+			foreach($this->subOptions[$value] as $subOption)
+			{
+				$subOption->register();
+			}
+		}
 	}
 
 	public function sanitize( $value ) {
@@ -37,7 +52,17 @@ class MultiOption extends Option {
 		foreach($this->values as $value => $name)
 		{
 			$id = 'sgdg-' . esc_attr( $this->name ) . '-value-' . esc_attr( $value );
-			echo ( '<label for="' . $id . '"><input type="radio" id="' . $id . '" name="' . esc_attr( $this->name ) . '" value="' . $value . '"' . ( $this->get() == $value ? ' checked' : '' ) . '>' . esc_html( $name ) . '</label><br>' );
+			echo( '<label for="' . $id . '"><input type="radio" id="' . $id . '" name="' . esc_attr( $this->name ) . '" value="' . $value . '"' . ( $this->get() == $value ? ' checked' : '' ) . '>' . esc_html( $name ) . '</label>' );
+			echo( '<br><table class="form-table"><tbody>' );
+			foreach($this->subOptions[$value] as $subOption)
+			{
+				echo( '<tr><th>' );
+				echo( $subOption->title );
+				echo( '</th><td>' );
+				$subOption->html();
+				echo( '</td></tr>' );
+			}
+			echo( '</tbody></table>' );
 		}
 	}
 }
