38805ea6a4b4c6957006ef611312046de2fc0ecd
GenaBitu <genabitu@gmail.com>
2018-06-12 22:54:24 +0200

    Added placeholder help links

--- plugin/admin/options-page.php
+++ plugin/admin/options-page.php
@@ -31,7 +31,11 @@ function html() {
 		return;
 	}
 
-	settings_errors( 'sgdg_messages' );
+	$help_link = 'https://napoveda.skaut.cz/dobryweb/skaut-google-drive-gallery'; // TODO: i18n
+	// translators: 1: Start of a help link 2: End of the help link
+	add_settings_error( 'sgdg', 'help', sprintf( esc_html__( 'See the %1$shelp%2$s for more information about how to configure the plugin.', 'skaut-google-drive-gallery' ), '<a href="' . esc_url( $help_link ) . '" target="_blank">', '</a>' ), 'notice-info' );
+
+	settings_errors( 'sgdg' );
 	echo( '<div class="wrap">' );
 	echo( '<h1>' . esc_html( get_admin_page_title() ) . '</h1>' );
 	echo( '<form action="options.php" method="post">' );
