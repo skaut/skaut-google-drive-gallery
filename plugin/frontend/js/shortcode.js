bb7101b25031db2f134692a3a7769f971fece8e0
GenaBitu <genabitu@gmail.com>
2018-07-02 23:03:02 +0200

    phpcs

--- plugin/frontend/js/shortcode.js
+++ plugin/frontend/js/shortcode.js
@@ -3,49 +3,49 @@ jQuery( document ).ready( function($) {
 	var reflow = function() {
 		var loaded = [];
 		var ratios = [];
-		$("#sgdg-gallery").children().each(function(i) {
-			$(this).css("position", "initial");
-			$(this).css("display", "inline-block");
+		$( "#sgdg-gallery" ).children().each(function(i) {
+			$( this ).css( "position", "initial" );
+			$( this ).css( "display", "inline-block" );
 			var val = this.getBoundingClientRect().width / this.getBoundingClientRect().height;
-			if($(this).find("svg").length > 0) {
-				var bbox = $(this).find("svg")[0].getBBox();
-				val = bbox.width / bbox.height;
+			if ($( this ).find( "svg" ).length > 0) {
+				var bbox = $( this ).find( "svg" )[0].getBBox();
+				val      = bbox.width / bbox.height;
 			}
-			if($(this).hasClass("sgdg-grid-square")) {
+			if ($( this ).hasClass( "sgdg-grid-square" )) {
 				val = 1;
 			}
-			if(isNaN(val)) {
+			if (isNaN( val )) {
 				loaded[i] = false;
 			} else {
 				loaded[i] = true;
-				ratios.push(val);
+				ratios.push( val );
 			}
-			$(this).css("position", "absolute");
+			$( this ).css( "position", "absolute" );
 		});
-		var config = {
-			containerWidth: $("#sgdg-gallery").width(),
-			boxSpacing: parseInt(sgdg_shortcode_localize.grid_spacing),
-			targetRowHeight: parseInt(sgdg_shortcode_localize.grid_height)
+		var config    = {
+			containerWidth: $( "#sgdg-gallery" ).width(),
+			boxSpacing: parseInt( sgdg_shortcode_localize.grid_spacing ),
+			targetRowHeight: parseInt( sgdg_shortcode_localize.grid_height )
 		};
-		var positions = require("justified-layout")(ratios, config);
-		var j = 0;
-		$("#sgdg-gallery").children().each(function(i) {
-			if(!loaded[i]) {
-				$(this).css("display", "none");
+		var positions = require( "justified-layout" )( ratios, config );
+		var j         = 0;
+		$( "#sgdg-gallery" ).children().each(function(i) {
+			if ( ! loaded[i]) {
+				$( this ).css( "display", "none" );
 				return;
 			}
 			var sizes = positions.boxes[j];
 			j++;
-			var containerPosition = $("#sgdg-gallery").position();
-			$(this).css("top", sizes.top + containerPosition.top);
-			$(this).css("left", sizes.left + containerPosition.left);
-			$(this).width(sizes.width);
-			$(this).height(sizes.height);
+			var containerPosition = $( "#sgdg-gallery" ).position();
+			$( this ).css( "top", sizes.top + containerPosition.top );
+			$( this ).css( "left", sizes.left + containerPosition.left );
+			$( this ).width( sizes.width );
+			$( this ).height( sizes.height );
 		});
-		$("#sgdg-gallery").height(positions.containerHeight)
+		$( "#sgdg-gallery" ).height( positions.containerHeight );
 	}
-	$(window).resize(reflow);
-	$("#sgdg-gallery").imagesLoaded(reflow);
+	$( window ).resize( reflow );
+	$( "#sgdg-gallery" ).imagesLoaded( reflow );
 	reflow();
 
 	$( "a[data-imagelightbox]" ).imageLightbox({
