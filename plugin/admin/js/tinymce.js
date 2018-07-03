4aa50dc998e3e34f1db88d4b7a012b19af79fd81
GenaBitu <genabitu@gmail.com>
2018-07-03 20:43:52 +0200

    eslint

--- plugin/admin/js/tinymce.js
+++ plugin/admin/js/tinymce.js
@@ -1,105 +1,101 @@
-"use strict";
-jQuery( document ).ready(function($) {
+'use strict';
+jQuery( document ).ready( function( $ ) {
 	var path = [];
+	var html = '<div id="sgdg-tinymce-modal"></div>';
 
-	$( "#sgdg-tinymce-button" ).click( tinymce_onclick );
-	var html = "<div id=\"sgdg-tinymce-modal\"></div>";
-	$( "body" ).append( html )
+	$( '#sgdg-tinymce-button' ).click( tinymceOnclick );
+	$( 'body' ).append( html );
 
-	function tinymce_onclick()
-	{
-		tinymce_html();
-		tb_show( sgdg_tinymce_localize.dialog_title, "#TB_inline?inlineId=sgdg-tinymce-modal" );
+	function tinymceOnclick() {
+		tinymceHtml();
+		tb_show( sgdg_tinymce_localize.dialog_title, '#TB_inline?inlineId=sgdg-tinymce-modal' );
 		path = [];
-		ajax_query();
+		ajaxQuery();
 	}
-	function tinymce_html()
-	{
-		var html = "<table id=\"sgdg-tinymce-table\" class=\"widefat\">";
-		html    += "<thead>";
-		html    += "<tr>";
-		html    += "<th class=\"sgdg-tinymce-path\">" + sgdg_tinymce_localize.root_name + "</th>";
-		html    += "</tr>";
-		html    += "</thead>";
-		html    += "<tbody id=\"sgdg-tinymce-list\"></tbody>";
-		html    += "<tfoot>";
-		html    += "<tr>";
-		html    += "<td class=\"sgdg-tinymce-path\">" + sgdg_tinymce_localize.root_name + "</td>";
-		html    += "</tr>";
-		html    += "</tfoot>";
-		html    += "</table>";
-		html    += "<div class=\"sgdg-tinymce-footer\">";
-		html    += "<a id=\"sgdg-tinymce-insert\" class=\"button button-primary\">" + sgdg_tinymce_localize.insert_button + "</a>";
-		html    += "</div>";
-		$( "#sgdg-tinymce-modal" ).html( html );
-		$( "#sgdg-tinymce-insert" ).click( function() {tinymce_submit();} )
+	function tinymceHtml() {
+		var html = '<table id="sgdg-tinymce-table" class="widefat">';
+		html += '<thead>';
+		html += '<tr>';
+		html += '<th class="sgdg-tinymce-path">' + sgdg_tinymce_localize.root_name + '</th>';
+		html += '</tr>';
+		html += '</thead>';
+		html += '<tbody id="sgdg-tinymce-list"></tbody>';
+		html += '<tfoot>';
+		html += '<tr>';
+		html += '<td class="sgdg-tinymce-path">' + sgdg_tinymce_localize.root_name + '</td>';
+		html += '</tr>';
+		html += '</tfoot>';
+		html += '</table>';
+		html += '<div class="sgdg-tinymce-footer">';
+		html += '<a id="sgdg-tinymce-insert" class="button button-primary">' + sgdg_tinymce_localize.insert_button + '</a>';
+		html += '</div>';
+		$( '#sgdg-tinymce-modal' ).html( html );
+		$( '#sgdg-tinymce-insert' ).click( function() {
+			tinymceSubmit();
+		});
 	}
 
-	function tinymce_submit()
-	{
-		if ($( "#sgdg-tinymce-insert" ).attr( "disabled" )) {
+	function tinymceSubmit() {
+		if ( $( '#sgdg-tinymce-insert' ).attr( 'disabled' ) ) {
 			return;
 		}
-		tinymce.activeEditor.insertContent( "[sgdg path=\"" + path.join( "/" ) + "\"]" );
+		tinymce.activeEditor.insertContent( '[sgdg path="' + path.join( '/' ) + '"]' );
 		tb_remove();
 	}
 
-	function ajax_query()
-	{
-		$( "#sgdg-tinymce-list" ).html( "" );
-		$( "#sgdg-tinymce-insert" ).attr( "disabled", "disabled" );
-		$.get(sgdg_tinymce_localize.ajax_url, {
-			_ajax_nonce: sgdg_tinymce_localize.nonce,
-			action: "list_gallery_dir",
+	function ajaxQuery() {
+		$( '#sgdg-tinymce-list' ).html( '' );
+		$( '#sgdg-tinymce-insert' ).attr( 'disabled', 'disabled' );
+		$.get( sgdg_tinymce_localize.ajax_url, {
+			_ajax_nonce: sgdg_tinymce_localize.nonce, // eslint-disable-line camelcase
+			action: 'list_gallery_dir',
 			path: path
-			}, function(data)
-			{
-				if (data.response) {
+			}, function( data ) {
+				if ( data.response ) {
 					success( data.response );
-				} else if (data.error) {
+				} else if ( data.error ) {
 					error( data.error );
 				}
 			}
 		);
 	}
 
-	function success(data)
-	{
-		$( "#sgdg-tinymce-insert" ).removeAttr( "disabled" );
-		var html = "";
-		if (path.length > 0) {
-			html += "<tr><td class=\"row-title\"><label>..</label></td></tr>";
-		}
+	function success( data ) {
+		var i;
+		var html = '';
 		var len = data.length;
-		for (var i = 0; i < len; i++) {
-			html += "<tr class=\"";
-			if ((path.length === 0 && i % 2 === 1) || (path.length > 0 && i % 2 === 0)) {
-				html += "alternate";
-			}
-			html += "\"><td class=\"row-title\"><label>" + data[i] + "</label></td></tr>";
+		$( '#sgdg-tinymce-insert' ).removeAttr( 'disabled' );
+		if ( 0 < path.length ) {
+			html += '<tr><td class="row-title"><label>..</label></td></tr>';
 		}
-		$( "#sgdg-tinymce-list" ).html( html );
+		for ( i = 0; i < len; i++ ) {
+			html += '<tr class="';
+			if ( ( 0 === path.length && 1 === i % 2 ) || ( 0 < path.length && 0 === i % 2 ) ) {
+				html += 'alternate';
+			}
+			html += '"><td class="row-title"><label>' + data[i] + '</label></td></tr>';
+		}
+		$( '#sgdg-tinymce-list' ).html( html );
 		html = sgdg_tinymce_localize.root_name;
-		len  = path.length;
-		for (i = 0; i < len; i++) {
-			html += " > ";
+		len = path.length;
+		for ( i = 0; i < len; i++ ) {
+			html += ' > ';
 			html += path[i];
 		}
-		$( ".sgdg-tinymce-path" ).html( html );
-		$( "#sgdg-tinymce-list label" ).click(function() {
+		$( '.sgdg-tinymce-path' ).html( html );
+		$( '#sgdg-tinymce-list label' ).click( function() {
 			var newDir = $( this ).html();
-			if (newDir === "..") {
+			if ( '..' === newDir ) {
 				path.pop();
 			} else {
 				path.push( newDir );
 			}
-			ajax_query();
+			ajaxQuery();
 		});
 	}
 
-	function error(message)
-	{
-		var html = "<div class=\"notice notice-error\"><p>" + message + "</p></div>";
-		$( "#TB_ajaxContent" ).html( html );
+	function error( message ) {
+		var html = '<div class="notice notice-error"><p>' + message + '</p></div>';
+		$( '#TB_ajaxContent' ).html( html );
 	}
 });
--- plugin/frontend/js/shortcode.js
+++ plugin/frontend/js/shortcode.js
@@ -1,63 +1,63 @@
-"use strict";
-jQuery( document ).ready( function($) {
+'use strict';
+jQuery( document ).ready( function( $ ) {
 	var reflow = function() {
+		var val, bbox, positions, sizes, containerPosition;
+		var j = 0;
 		var loaded = [];
 		var ratios = [];
-		$( "#sgdg-gallery" ).children().each(function(i) {
-			$( this ).css( "position", "initial" );
-			$( this ).css( "display", "inline-block" );
-			var val = this.getBoundingClientRect().width / this.getBoundingClientRect().height;
-			if ($( this ).find( "svg" ).length > 0) {
-				var bbox = $( this ).find( "svg" )[0].getBBox();
-				val      = bbox.width / bbox.height;
+		$( '#sgdg-gallery' ).children().each( function( i ) {
+			$( this ).css( 'position', 'initial' );
+			$( this ).css( 'display', 'inline-block' );
+			val = this.getBoundingClientRect().width / this.getBoundingClientRect().height;
+			if ( 0 < $( this ).find( 'svg' ).length ) {
+				bbox = $( this ).find( 'svg' )[0].getBBox();
+				val = bbox.width / bbox.height;
 			}
-			if ($( this ).hasClass( "sgdg-grid-square" )) {
+			if ( $( this ).hasClass( 'sgdg-grid-square' ) ) {
 				val = 1;
 			}
-			if (isNaN( val )) {
+			if ( isNaN( val ) ) {
 				loaded[i] = false;
 			} else {
 				loaded[i] = true;
 				ratios.push( val );
 			}
-			$( this ).css( "position", "absolute" );
+			$( this ).css( 'position', 'absolute' );
 		});
-		var config    = {
-			containerWidth: $( "#sgdg-gallery" ).width(),
+		positions = require( 'justified-layout' )( ratios, {
+			containerWidth: $( '#sgdg-gallery' ).width(),
 			boxSpacing: parseInt( sgdg_shortcode_localize.grid_spacing ),
 			targetRowHeight: parseInt( sgdg_shortcode_localize.grid_height )
-		};
-		var positions = require( "justified-layout" )( ratios, config );
-		var j         = 0;
-		$( "#sgdg-gallery" ).children().each(function(i) {
+		});
+		$( '#sgdg-gallery' ).children().each( function( i ) {
 			if ( ! loaded[i]) {
-				$( this ).css( "display", "none" );
+				$( this ).css( 'display', 'none' );
 				return;
 			}
-			var sizes = positions.boxes[j];
+			sizes = positions.boxes[j];
 			j++;
-			var containerPosition = $( "#sgdg-gallery" ).position();
-			$( this ).css( "top", sizes.top + containerPosition.top );
-			$( this ).css( "left", sizes.left + containerPosition.left );
+			containerPosition = $( '#sgdg-gallery' ).position();
+			$( this ).css( 'top', sizes.top + containerPosition.top );
+			$( this ).css( 'left', sizes.left + containerPosition.left );
 			$( this ).width( sizes.width );
 			$( this ).height( sizes.height );
 		});
-		$( "#sgdg-gallery" ).height( positions.containerHeight );
-	}
+		$( '#sgdg-gallery' ).height( positions.containerHeight );
+	};
 	$( window ).resize( reflow );
-	$( "#sgdg-gallery" ).imagesLoaded( reflow );
+	$( '#sgdg-gallery' ).imagesLoaded( reflow );
 	reflow();
 
-	$( "a[data-imagelightbox]" ).imageLightbox({
-		allowedTypes: "",
+	$( 'a[data-imagelightbox]' ).imageLightbox({
+		allowedTypes: '',
 		animationSpeed: parseInt( sgdg_shortcode_localize.preview_speed, 10 ),
-		activity: (sgdg_shortcode_localize.preview_activity === "true"),
-		arrows: (sgdg_shortcode_localize.preview_arrows === "true"),
-		button: (sgdg_shortcode_localize.preview_closebutton === "true"),
+		activity: ( 'true' === sgdg_shortcode_localize.preview_activity ),
+		arrows: ( 'true' === sgdg_shortcode_localize.preview_arrows ),
+		button: ( 'true' === sgdg_shortcode_localize.preview_closebutton ),
 		fullscreen: true,
 		history: true,
 		overlay: true,
-		quitOnEnd: (sgdg_shortcode_localize.preview_quitOnEnd === "true")
+		quitOnEnd: ( 'true' === sgdg_shortcode_localize.preview_quitOnEnd )
 	});
 
 });
