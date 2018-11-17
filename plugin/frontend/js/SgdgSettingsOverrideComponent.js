d62d3e0a4ad0080351aaf6b5a95a02098d6953bb
GenaBitu <genabitu@gmail.com>
2018-11-17 15:10:19 +0100

    Overriding most settings

--- plugin/frontend/js/SgdgSettingsOverrideComponent.js
+++ plugin/frontend/js/SgdgSettingsOverrideComponent.js
@@ -9,6 +9,12 @@ SgdgSettingsOverrideComponent.prototype.render = function() {
 	return el( wp.components.PanelBody, {title: sgdgBlockLocalize.settings_override, className: 'sgdg-block-settings'}, [
 		el( SgdgIntegerSettingsComponent, {block: this.block, name: 'grid_height'}),
 		el( SgdgIntegerSettingsComponent, {block: this.block, name: 'grid_spacing'}),
-		el( SgdgBooleanSettingsComponent, {block: this.block, name: 'dir_counts'})
+		el( SgdgBooleanSettingsComponent, {block: this.block, name: 'dir_counts'}),
+		el( SgdgIntegerSettingsComponent, {block: this.block, name: 'preview_size'}),
+		el( SgdgIntegerSettingsComponent, {block: this.block, name: 'preview_speed'}),
+		el( SgdgBooleanSettingsComponent, {block: this.block, name: 'preview_arrows'}),
+		el( SgdgBooleanSettingsComponent, {block: this.block, name: 'preview_close_button'}),
+		el( SgdgBooleanSettingsComponent, {block: this.block, name: 'preview_loop'}),
+		el( SgdgBooleanSettingsComponent, {block: this.block, name: 'preview_activity_indicator'})
 	]);
 };
