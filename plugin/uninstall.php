fe74402d1d63e5055e688c128ade27d7ee230c80
GenaBitu <genabitu@gmail.com>
2018-11-21 18:49:43 +0100

    Updated uninstall script

--- plugin/uninstall.php
+++ plugin/uninstall.php
@@ -5,20 +5,24 @@ defined( 'WP_UNINSTALL_PLUGIN' ) || die( 'Die, die, die!' );
 delete_option( 'sgdg_client_id' );
 delete_option( 'sgdg_client_secret' );
 delete_option( 'sgdg_access_token' );
+
 delete_option( 'sgdg_root_path' );
+
+delete_option( 'sgdg_grid_height' );
+delete_option( 'sgdg_grid_spacing' );
+delete_option( 'sgdg_dir_title_size' );
+delete_option( 'sgdg_dir_counts' );
+delete_option( 'sgdg_image_ordering_order' );
+delete_option( 'sgdg_image_ordering_by' );
+delete_option( 'sgdg_dir_ordering_order' );
+delete_option( 'sgdg_dir_ordering_by' );
+
 delete_option( 'sgdg_preview_size' );
 delete_option( 'sgdg_preview_speed' );
 delete_option( 'sgdg_preview_arrows' );
 delete_option( 'sgdg_preview_closebutton' );
 delete_option( 'sgdg_preview_loop' );
 delete_option( 'sgdg_preview_activity' );
-delete_option( 'sgdg_dir_counts' );
-delete_option( 'sgdg_image_ordering_order' );
-delete_option( 'sgdg_image_ordering_by' );
-delete_option( 'sgdg_dir_ordering_order' );
-delete_option( 'sgdg_dir_ordering_by' );
-delete_option( 'sgdg_grid_height' );
-delete_option( 'sgdg_grid_spacing' );
 
 // Deprecated
 delete_option( 'sgdg_image_ordering' );
