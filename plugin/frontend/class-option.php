d87a0ac1df58f5e929da07c347a130555b038112
GenaBitu <genabitu@gmail.com>
2019-06-06 15:39:53 +0200

    Documented Option

--- plugin/frontend/class-option.php
+++ plugin/frontend/class-option.php
@@ -1,13 +1,62 @@
 <?php
+/**
+ * Contains the Option iterface
+ *
+ * This class serves as an interface for all options of the plugin - each option is configurable in some section of some page of the settings, has a name, default value, getter etc.
+ *
+ * @package skaut-google-drive-gallery
+ */
+
 namespace Sgdg\Frontend;
 
+/**
+ * An interface for all plugin options
+ *
+ * This class serves as an interface for all options of the plugin - each option is configurable in some section of some page of the settings, has a name, default value, getter etc.
+ */
 abstract class Option {
+	/**
+	 * The name of the option to be used as the key to reference it.
+	 *
+	 * @var string $name
+	 */
 	protected $name;
+	/**
+	 * The default value of the option to be returned if the option is not set.
+	 *
+	 * @var any $default_value
+	 */
 	protected $default_value;
+	/**
+	 * The page in which the option will be accessible to the user.
+	 *
+	 * @var string $page
+	 */
 	protected $page;
+	/**
+	 * The section (within the selected page) in which the option will be accessible to the user.
+	 *
+	 * @var string $section
+	 */
 	protected $section;
+	/**
+	 * A human-readable name of the option to be displayed to the user.
+	 *
+	 * @var string $title
+	 */
 	protected $title;
 
+	/**
+	 * Option class constructor.
+	 *
+	 * This constructor is intended to be used by sub-classes as the Option class is abstract.
+	 *
+	 * @param string $name The name of the option to be used as the key to reference it. The prefix `sgdg_` will be added automatically.
+	 * @param any    $default_value The default value of the option to be returned if the option is not set.
+	 * @param string $page The page in which the option will be accessible to the user. The prefix `sgdg_` will be added automatically.
+	 * @param string $section The section (within the selected page) in which the option will be accessible to the user. The prefix `sgdg_` will be added automatically.
+	 * @param string $title A human-readable name of the option to be displayed to the user.
+	 */
 	public function __construct( $name, $default_value, $page, $section, $title ) {
 		$this->name = 'sgdg_' . $name;
 		// @phan-suppress-next-line PhanTypeMismatchProperty
@@ -17,23 +66,67 @@ abstract class Option {
 		$this->title         = $title;
 	}
 
+	/**
+	 * Register the setting with WordPress.
+	 */
 	abstract public function register();
 
+	/**
+	 * Sanitize user input.
+	 *
+	 * This function should sanitize user input for the option (invalid values, values outside bounds etc.). This function should be registered as a `sanitize_callback` when registering the option.
+	 *
+	 * @see register()
+	 *
+	 * @param any $value The unsanitized user input.
+	 * @return any The sanitized value to be written to the database.
+	 */
 	public function sanitize( $value ) {
 		return $value;
 	}
 
+	/**
+	 * Add the option to the WordPress UI.
+	 *
+	 * This function adds the the option to the WordPress settings on page `$page` in section `$section`. The option is drawn by the `html()` method.
+	 *
+	 * @see $page
+	 * @see $section
+	 * @see html()
+	 */
 	public function add_field() {
 		$this->register();
 		add_settings_field( $this->name, $this->title, [ $this, 'html' ], $this->page, $this->section );
 	}
 
+	/**
+	 * Render the UI for updating the option.
+	 *
+	 * This function renders (by calling `echo()`) the UI for updating the option, including the current value.
+	 */
 	abstract public function html();
 
+	/**
+	 * Get the value of the option.
+	 *
+	 * Returns the value of the option, or a default value if it isn't defined.
+	 *
+	 * @see $default_value
+	 *
+	 * @param any $default_value The default value to be returned if the option isn't defined. If it is null, the $default_value property will be used instead. Default null.
+	 * @return The value of the option.
+	 */
 	public function get( $default_value = null ) {
 		return get_option( $this->name, ( isset( $default_value ) ? $default_value : $this->default_value ) );
 	}
 
+	/**
+	 * Get the title of the option.
+	 *
+	 * @see $title
+	 *
+	 * @return string The title in English.
+	 */
 	public function get_title() {
 		return $this->title;
 	}
