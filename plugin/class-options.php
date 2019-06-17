9a4e58c87e06651699fef5bb043dcb3543904b6a
GenaBitu <developer@dedic.eu>
2019-06-17 13:01:04 +0200

    Fixed docs of options

--- plugin/class-options.php
+++ plugin/class-options.php
@@ -48,118 +48,118 @@ class Options {
 	/**
 	 * The client ID of the Google app.
 	 *
-	 * @var \Sgdg\Admin\StringCodeOption $client_id
+	 * @var \Sgdg\Frontend\StringCodeOption $client_id
 	 */
 	public static $client_id;
 	/**
 	 * The client secret of the Google app.
 	 *
-	 * @var \Sgdg\Admin\StringCodeOption $client_secret
+	 * @var \Sgdg\Frontend\StringCodeOption $client_secret
 	 */
 	public static $client_secret;
 
 	/**
 	 * The root path of the plugin. This is the only directory the plugin should ever touch.
 	 *
-	 * @var \Sgdg\Admin\RootPathOption $root_path
+	 * @var \Sgdg\Frontend\RootPathOption $root_path
 	 */
 	public static $root_path;
 
 	/**
 	 * The height of a row in the image grid.
 	 *
-	 * @var \Sgdg\Admin\BoundedIntegerOption $grid_height
+	 * @var \Sgdg\Frontend\BoundedIntegerOption $grid_height
 	 */
 	public static $grid_height;
 	/**
 	 * Item spacing in the image grid.
 	 *
-	 * @var \Sgdg\Admin\IntegerOption $grid_spacing
+	 * @var \Sgdg\Frontend\IntegerOption $grid_spacing
 	 */
 	public static $grid_spacing;
 	/**
 	 * Directory title size, including CSS units.
 	 *
-	 * @var \Sgdg\Admin\StringOption $dir_title_size
+	 * @var \Sgdg\Frontend\StringOption $dir_title_size
 	 */
 	public static $dir_title_size;
 	/**
 	 * Whether to show directory item counts. Accepts `true`, `false`.
 	 *
-	 * @var \Sgdg\Admin\BooleanOption $dir_counts
+	 * @var \Sgdg\Frontend\BooleanOption $dir_counts
 	 */
 	public static $dir_counts;
 	/**
 	 * Number of items per 1 page.
 	 *
-	 * @var \Sgdg\Admin\BoundedIntegerOption $page_size
+	 * @var \Sgdg\Frontend\BoundedIntegerOption $page_size
 	 */
 	public static $page_size;
 	/**
 	 * Whether to autoload new images. Accepts `true`, `false`.
 	 *
-	 * @var \Sgdg\Admin\BoundedIntegerOption $page_autoload
+	 * @var \Sgdg\Frontend\BoundedIntegerOption $page_autoload
 	 */
 	public static $page_autoload;
 	/**
 	 * How to order images in the gallery.
 	 *
-	 * @var \Sgdg\Admin\OrderingOption $image_ordering
+	 * @var \Sgdg\Frontend\OrderingOption $image_ordering
 	 */
 	public static $image_ordering;
 	/**
 	 * How to order directories in the gallery.
 	 *
-	 * @var \Sgdg\Admin\OrderingOption $dir_ordering
+	 * @var \Sgdg\Frontend\OrderingOption $dir_ordering
 	 */
 	public static $dir_ordering;
 	/**
 	 * A prefix separator to cut a prefix from the start of all directory names.
 	 *
-	 * @var \Sgdg\Admin\StringOption $dir_prefix
+	 * @var \Sgdg\Frontend\StringOption $dir_prefix
 	 */
 	public static $dir_prefix;
 
 	/**
 	 * Maximum size of an image in the lightbox.
 	 *
-	 * @var \Sgdg\Admin\BoundedIntegerOption $preview_size
+	 * @var \Sgdg\Frontend\BoundedIntegerOption $preview_size
 	 */
 	public static $preview_size;
 	/**
 	 * Lightbox animation speed.
 	 *
-	 * @var \Sgdg\Admin\BoundedIntegerOption $preview_speed
+	 * @var \Sgdg\Frontend\BoundedIntegerOption $preview_speed
 	 */
 	public static $preview_speed;
 	/**
 	 * Whether to show lightbox navigation arrows.
 	 *
-	 * @var \Sgdg\Admin\BooleanOption $preview_arrows
+	 * @var \Sgdg\Frontend\BooleanOption $preview_arrows
 	 */
 	public static $preview_arrows;
 	/**
 	 * Whether to show lightbox close button.
 	 *
-	 * @var \Sgdg\Admin\BooleanOption $preview_close_button
+	 * @var \Sgdg\Frontend\BooleanOption $preview_close_button
 	 */
 	public static $preview_close_button;
 	/**
 	 * Whether to loop the images in the lightbox.
 	 *
-	 * @var \Sgdg\Admin\BooleanOption $preview_loop
+	 * @var \Sgdg\Frontend\BooleanOption $preview_loop
 	 */
 	public static $preview_loop;
 	/**
 	 * Whether to show an activity indicator while the lightbox is loading.
 	 *
-	 * @var \Sgdg\Admin\BooleanOption $preview_activity_indicator
+	 * @var \Sgdg\Frontend\BooleanOption $preview_activity_indicator
 	 */
 	public static $preview_activity_indicator;
 	/**
 	 * Whether to show image captions in the lightbox.
 	 *
-	 * @var \Sgdg\Admin\BooleanOption $preview_captions
+	 * @var \Sgdg\Frontend\BooleanOption $preview_captions
 	 */
 	public static $preview_captions;
 
