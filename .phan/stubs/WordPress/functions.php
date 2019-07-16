849a7501df040a1c7c9008d384ae50acce7eb38f
GenaBitu <developer@dedic.eu>
2019-07-16 16:52:11 +0200

    Ordered phan stubs

--- .phan/stubs/WordPress/functions.php
+++ .phan/stubs/WordPress/functions.php
@@ -11,100 +11,82 @@
 const DAY_IN_SECONDS = null;
 const WP_PLUGIN_DIR  = null;
 
-function add_menu_page( $a, $b, $c, $d, $e, $f ) {
-}
-
-function wp_verify_nonce( $a, $b ) {
-}
-
-function add_submenu_page( $a, $b, $c, $d, $e, $f ) {
-}
-
-function settings_errors() {
-}
-
-function esc_html( $a ) {
-	return '';
-}
-
-function get_admin_page_title() {
-}
-
-function wp_nonce_field( $a ) {
-}
-
-function do_settings_sections( $a ) {
-}
-
-function submit_button( $a ) {
-}
-
-function wp_nonce_url( $a, $b ) {
-}
-
-function add_settings_section( $a, $b, $c, $d ) {
-}
-
 function __( $a, $b ) {
+}
+
+function add_action( $a, $b ) {
+}
+
+function add_menu_page( $a, $b, $c, $d, $e, $f ) {
 }
 
 function add_settings_error( $a, $b, $c, $d ) {
 }
 
-function get_settings_errors() {
-	return [];
+function add_settings_field( $a, $b, $c, $d, $e ) {
 }
 
-function is_admin() {
-	return false;
+function add_settings_section( $a, $b, $c, $d ) {
+}
+
+function add_shortcode( $a, $b ) {
+}
+
+function add_submenu_page( $a, $b, $c, $d, $e, $f ) {
 }
 
 function add_thickbox() {
 }
 
-function get_user_option( $a ) {
+function admin_url( $a ) {
 }
 
 function check_ajax_referer( $a ) {
+}
+
+function checked( $a, $b ) {
 }
 
 function current_user_can( $a ) {
 	return false;
 }
 
-function get_site_url() {
+function deactivate_plugins( $a ) {
 }
 
-function wp_parse_url( $a ) {
-	return [
-		'host'   => '',
-		'scheme' => '',
-	];
+function delete_option( $a ) {
 }
 
-function wp_create_nonce( $a ) {
+function delete_transient( $a ) {
 }
 
-function register_block_type( $a, $b ) {
-}
-
-function checked( $a, $b ) {
-}
-
-function wp_json_encode( $a, $b ) {
-}
-
-function add_settings_field( $a, $b, $c, $d, $e ) {
-}
-
-function register_setting( $a, $b, $c ) {
+function do_settings_sections( $a ) {
 }
 
 function esc_attr( $a ) {
 	return '';
 }
 
+function esc_html( $a ) {
+	return '';
+}
+
+function esc_html__( $a, $b ) {
+	return '';
+}
+
+function esc_url( $a ) {
+	return '';
+}
+
 function esc_url_raw( $a ) {
+	return '';
+}
+
+function get_admin_page_title() {
+}
+
+function get_locale() {
 	return '';
 }
 
@@ -119,38 +101,11 @@ function get_option( $a, $b ) {
 	}
 }
 
-function update_option( $a, $b ) {
+function get_settings_errors() {
+	return [];
 }
 
-function wp_send_json( $a ) {
-}
-
-function add_shortcode( $a, $b ) {
-}
-
-function wp_localize_script( $a, $b, $c ) {
-}
-
-function wp_add_inline_style( $a, $b ) {
-}
-
-function register_activation_hook( $a, $b ) {
-}
-
-function add_action( $a, $b ) {
-}
-
-function deactivate_plugins( $a ) {
-}
-
-function plugin_basename( $a ) {
-}
-
-// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-function wp_die( $a ) {
-}
-
-function set_transient( $a, $b, $c ) {
+function get_site_url() {
 }
 
 function get_transient( $a ) {
@@ -165,31 +120,55 @@ function get_transient( $a ) {
 	}
 }
 
-function get_locale() {
-	return '';
+function get_user_option( $a ) {
 }
 
-function admin_url( $a ) {
+function is_admin() {
+	return false;
 }
 
-function esc_html__( $a, $b ) {
-	return '';
-}
-
-function esc_url( $a ) {
-	return '';
-}
-
-function delete_transient( $a ) {
+function plugin_basename( $a ) {
 }
 
 function plugins_url( $a ) {
 }
 
-function wp_register_script( $a, $b, $c, $d, $e ) {
+function register_activation_hook( $a, $b ) {
 }
 
-function wp_register_style( $a, $b, $c, $d ) {
+function register_block_type( $a, $b ) {
+}
+
+function register_setting( $a, $b, $c ) {
+}
+
+function sanitize_text_field( $a ) {
+	return '';
+}
+
+function set_transient( $a, $b, $c ) {
+}
+
+function settings_errors() {
+}
+
+function submit_button( $a ) {
+}
+
+function tests_add_filter( $a, $b ) {
+}
+
+function update_option( $a, $b ) {
+}
+
+function wp_add_inline_style( $a, $b ) {
+}
+
+function wp_create_nonce( $a ) {
+}
+
+// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+function wp_die( $a ) {
 }
 
 function wp_enqueue_script( $a ) {
@@ -198,11 +177,35 @@ function wp_enqueue_script( $a ) {
 function wp_enqueue_style( $a ) {
 }
 
-function delete_option( $a ) {
+function wp_json_encode( $a, $b ) {
 }
 
-function sanitize_text_field( $a ) {
-	return '';
+function wp_localize_script( $a, $b, $c ) {
+}
+
+function wp_nonce_field( $a ) {
+}
+
+function wp_nonce_url( $a, $b ) {
+}
+
+function wp_parse_url( $a ) {
+	return [
+		'host'   => '',
+		'scheme' => '',
+	];
+}
+
+function wp_rand( $a, $b ) {
+}
+
+function wp_register_script( $a, $b, $c, $d, $e ) {
+}
+
+function wp_register_style( $a, $b, $c, $d ) {
+}
+
+function wp_send_json( $a ) {
 }
 
 function wp_unslash( $a ) {
@@ -214,8 +217,5 @@ function wp_unslash( $a ) {
 	}
 }
 
-function tests_add_filter( $a, $b ) {
-}
-
-function wp_rand( $a, $b ) {
+function wp_verify_nonce( $a, $b ) {
 }
