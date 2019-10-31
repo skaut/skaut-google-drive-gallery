8b72132e97b66823f19c9f12d7feb38aaeb56b16
GenaBitu <developer@dedic.eu>
2019-10-31 18:18:27 +0100

    Remade SgdgSettingsComponent into a TS class

--- src/ts/frontend/SgdgSettingsComponent.ts
+++ src/ts/frontend/SgdgSettingsComponent.ts
@@ -1,36 +1,48 @@
 'use strict';
 var el = wp.element.createElement;
 
-var SgdgSettingsComponent = function( attributes ) {
-	var value;
-	this.block = attributes.block;
-	this.name = attributes.name;
-	value = this.block.getAttribute( this.name );
-	if ( undefined === value ) {
-		value = sgdgBlockLocalize[this.name].default;
+abstract class SgdgSettingsComponent extends wp.element.Component<any, any> {
+	protected block: any;
+	protected name: any;
+
+	public constructor( props ) {
+		super( props );
+		var value;
+		this.block = props.block;
+		this.name = props.name;
+		value = this.block.getAttribute( this.name );
+		if ( undefined === value ) {
+			value = sgdgBlockLocalize[this.name].default;
+		}
+		this.state = {value: value};
 	}
-	this.state = {value: value};
-};
-SgdgSettingsComponent.prototype = Object.create( wp.element.Component.prototype );
-SgdgSettingsComponent.prototype.render = function() {
-	var that = this;
-	var value = this.block.getAttribute( this.name );
-	return el( 'div', {className: 'sgdg-block-settings-row'}, [
-		el( wp.components.ToggleControl, {checked: undefined !== value, className: 'sgdg-block-settings-checkbox', onChange: function() {
-			that.toggle();
-		}}),
-		el( 'span', {className: 'sgdg-block-settings-description'}, [
-			sgdgBlockLocalize[this.name].name,
-			':'
-		]),
-		this.renderInput()
-	]);
-};
-SgdgSettingsComponent.prototype.toggle = function() {
-	this.block.setAttribute( this.name, undefined !== this.block.getAttribute( this.name ) ? undefined : this.state.value );
-};
-SgdgSettingsComponent.prototype.change = function( e ) {
-	var value = this.getValue( e.target );
-	this.setState({value: value});
-	this.block.setAttribute( this.name, undefined === value ? sgdgBlockLocalize[this.name].default : value );
-};
+
+	public render() {
+		var that = this;
+		var value = this.block.getAttribute( this.name );
+		return el( 'div', {className: 'sgdg-block-settings-row'}, [
+			el( wp.components.ToggleControl, {checked: undefined !== value, className: 'sgdg-block-settings-checkbox', onChange: function() {
+				that.toggle();
+			}}),
+			el( 'span', {className: 'sgdg-block-settings-description'}, [
+				sgdgBlockLocalize[this.name].name,
+				':'
+			]),
+			this.renderInput()
+		]);
+	}
+
+	protected abstract renderInput();
+
+	private toggle() {
+		this.block.setAttribute( this.name, undefined !== this.block.getAttribute( this.name ) ? undefined : this.state.value );
+	}
+
+	protected change( e ) {
+		var value = this.getValue( e.target );
+		this.setState({value: value});
+		this.block.setAttribute( this.name, undefined === value ? sgdgBlockLocalize[this.name].default : value );
+	}
+
+	protected abstract getValue( _: any );
+}
