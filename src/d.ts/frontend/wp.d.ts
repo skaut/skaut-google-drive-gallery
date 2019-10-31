4eaffd5adc4d547becd02eab542f945801ab71ef
GenaBitu <developer@dedic.eu>
2019-10-31 13:34:58 +0100

    Added wp.components

--- src/d.ts/frontend/wp.d.ts
+++ src/d.ts/frontend/wp.d.ts
@@ -1,5 +1,6 @@
 declare var wp: {
 	blocks: typeof import('wordpress__blocks');
+	components: typeof import('wordpress__components');
 	editor: typeof import('wordpress__editor');
 	element: typeof import('wordpress__element');
 };
