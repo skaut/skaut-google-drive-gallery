70ee31ed632e4a6052351f622f3081d68a1a0fe5
GenaBitu <developer@dedic.eu>
2019-10-31 19:04:52 +0100

    Remade SgdgBooleanSettingsComponent into a TS class

--- src/ts/frontend/SgdgBooleanSettingsComponent.ts
+++ src/ts/frontend/SgdgBooleanSettingsComponent.ts
@@ -1,17 +1,16 @@
 'use strict';
 var el = wp.element.createElement;
 
-var SgdgBooleanSettingsComponent = function( attributes ) {
-	SgdgSettingsComponent.call( this, attributes );
-};
-SgdgBooleanSettingsComponent.prototype = Object.create( SgdgSettingsComponent.prototype );
-SgdgBooleanSettingsComponent.prototype.renderInput = function() {
-	var that = this;
-	var value = this.block.getAttribute( this.name );
-	return el( 'input', {checked: 'true' === this.state.value, className: 'sgdg-block-settings-boolean', disabled: undefined === value, onChange: function( e ) {
-		that.change( e );
-	}, type: 'checkbox'});
-};
-SgdgBooleanSettingsComponent.prototype.getValue = function( element ) {
-	return element.checked ? 'true' : 'false';
-};
+class SgdgBooleanSettingsComponent extends SgdgSettingsComponent {
+	protected renderInput() {
+		var that = this;
+		var value = this.block.getAttribute( this.name );
+		return el( 'input', {checked: 'true' === this.state.value, className: 'sgdg-block-settings-boolean', disabled: undefined === value, onChange: function( e ) {
+			that.change( e );
+		}, type: 'checkbox'});
+	}
+
+	protected getValue( element ) {
+		return element.checked ? 'true' : 'false';
+	};
+}
