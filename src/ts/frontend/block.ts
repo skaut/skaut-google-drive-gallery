0153d19a9d86ebad00c41954dce2107ff3f5547f
GenaBitu <developer@dedic.eu>
2019-11-01 14:03:37 +0100

    Updated ignores

--- src/ts/frontend/block.ts
+++ src/ts/frontend/block.ts
@@ -8,37 +8,37 @@ wp.blocks.registerBlockType( 'skaut-google-drive-gallery/gallery', {
 			type: 'array',
 			default: [],
 		},
-		grid_height: { // eslint-disable-line camelcase
+		grid_height: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'number',
 		},
-		grid_spacing: { // eslint-disable-line camelcase
+		grid_spacing: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'number',
 		},
-		dir_counts: { // eslint-disable-line camelcase
+		dir_counts: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'string',
 		},
-		page_size: { // eslint-disable-line camelcase
+		page_size: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'number',
 		},
-		page_autoload: { // eslint-disable-line camelcase
+		page_autoload: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'string',
 		},
-		image_ordering_order: { // eslint-disable-line camelcase
+		image_ordering_order: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'string',
 		},
-		image_ordering_by: { // eslint-disable-line camelcase
+		image_ordering_by: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'string',
 		},
-		dir_ordering_order: { // eslint-disable-line camelcase
+		dir_ordering_order: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'string',
 		},
-		dir_ordering_by: { // eslint-disable-line camelcase
+		dir_ordering_by: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'string',
 		},
-		preview_size: { // eslint-disable-line camelcase
+		preview_size: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'number',
 		},
-		preview_loop: { // eslint-disable-line camelcase
+		preview_loop: { // eslint-disable-line @typescript-eslint/camelcase
 			type: 'string',
 		},
 	},
