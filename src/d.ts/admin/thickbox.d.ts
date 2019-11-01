11d29593554c05a270f50cc7ccc0f3fd04b26a74
GenaBitu <developer@dedic.eu>
2019-11-01 15:53:50 +0100

    Fixed last eslint errors

--- src/d.ts/admin/thickbox.d.ts
+++ src/d.ts/admin/thickbox.d.ts
@@ -1,3 +1,3 @@
-declare function tb_show( caption: string, url: string, imageGroup?: string ): void
+declare function tb_show( caption: string, url: string, imageGroup?: string ): void // eslint-disable-line @typescript-eslint/camelcase
 
-declare function tb_remove(): false
+declare function tb_remove(): false // eslint-disable-line @typescript-eslint/camelcase
