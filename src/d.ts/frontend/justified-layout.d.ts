a1e4d2fdd607b5467953ac93e2978d421c06f931
GenaBitu <developer@dedic.eu>
2019-11-01 15:34:11 +0100

    Fixed declarations for justified-layout

--- src/d.ts/frontend/justified-layout.d.ts
+++ src/d.ts/frontend/justified-layout.d.ts
@@ -1,4 +1,10 @@
-declare interface JustifiedLayoutResult {
-	boxes: any;
-	containerHeight: any;
+declare module 'justified-layout' {
+	interface Result {
+		boxes: any;
+		containerHeight: any;
+	}
+
+	function convertSizesToAspectRatios( input: any, config: any ): any;
+
+	export = convertSizesToAspectRatios;
 }
