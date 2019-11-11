b2fa64a8a9cca2c1d28aa93a196dde9dee1bfa92
GenaBitu <developer@dedic.eu>
2019-11-11 19:14:19 +0100

    Refactored ShortcodeRegistry into a const

--- src/ts/frontend/shortcode.ts
+++ src/ts/frontend/shortcode.ts
@@ -339,10 +339,18 @@ jQuery( document ).ready( function( $ ) {
 		}
 	}
 
-	class ShortcodeRegistry {
-		private static shortcodes: Record<string, Shortcode> = {};
+	interface ShortcodeRegistry {
+		shortcodes: Record<string, Shortcode>;
+		init(): void;
+		reflowAll(): void;
+		onLightboxNavigation( e: JQuery ): void;
+		removePageFromHistory(): void;
+	}
 
-		public static init(): void {
+	const ShortcodeRegistry: ShortcodeRegistry = {
+		shortcodes: {},
+
+		init(): void {
 			$( '.sgdg-gallery-container' ).each( function() {
 				const container = $( this );
 				const hash = container.data( 'sgdgHash' );
@@ -351,23 +359,23 @@ jQuery( document ).ready( function( $ ) {
 
 			$( document ).on( 'start.ilb2 next.ilb2 previous.ilb2', ( _, e ) => ShortcodeRegistry.onLightboxNavigation( e ) );
 			$( document ).on( 'quit.ilb2', () => ShortcodeRegistry.removePageFromHistory() );
-		}
+		},
 
-		public static reflowAll(): void {
+		reflowAll(): void {
 			$.each( this.shortcodes, function( _, shortcode ) {
 				shortcode.reflow();
 			} );
-		}
+		},
 
-		private static onLightboxNavigation( e: JQuery ): void {
+		onLightboxNavigation( e: JQuery ): void {
 			const hash = $( e ).data( 'imagelightbox' );
 			ShortcodeRegistry.shortcodes[ hash ].onLightboxNavigation( e );
-		}
+		},
 
-		private static removePageFromHistory(): void {
+		removePageFromHistory(): void {
 			history.replaceState( history.state, '', removeQueryParameter( '[^-]+', 'page' ) );
-		}
-	}
+		},
+	};
 
 	ShortcodeRegistry.init();
 } );
