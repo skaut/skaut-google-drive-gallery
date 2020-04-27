b31b8d821f5d8c88d33aa8141f3a69b592a4d3bf
Marek Dědič <developer@dedic.eu>
2020-04-27 11:10:39 +0200

    Intl polyfill added

--- src/php/bundled/vendor-includes.php
+++ src/php/bundled/vendor-includes.php
@@ -19,6 +19,9 @@ require_once __DIR__ . '/vendor/psr/log/Psr/Log/LoggerInterface.php';
 require_once __DIR__ . '/vendor/psr/cache/src/CacheItemPoolInterface.php';
 require_once __DIR__ . '/vendor/psr/cache/src/CacheItemInterface.php';
 
+require_once __DIR__ . '/vendor/symfony/polyfill-intl-idn/Idn.php';
+require_once __DIR__ . '/vendor/symfony/polyfill-intl-idn/bootstrap.php';
+
 require_once __DIR__ . '/vendor/guzzlehttp/guzzle/src/Exception/GuzzleException.php';
 require_once __DIR__ . '/vendor/guzzlehttp/guzzle/src/Exception/TransferException.php';
 require_once __DIR__ . '/vendor/guzzlehttp/guzzle/src/Exception/RequestException.php';
