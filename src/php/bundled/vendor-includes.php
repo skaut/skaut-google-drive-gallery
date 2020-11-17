c11c761519a5255a4f3f1404e3cd25c637f75491
Marek Dědič <developer@dedic.eu>
2020-11-17 15:16:31 +0100

    Fixed missing includes

--- src/php/bundled/vendor-includes.php
+++ src/php/bundled/vendor-includes.php
@@ -54,18 +54,23 @@ require_once __DIR__ . '/vendor/guzzlehttp/guzzle/src/Utils.php';
 require_once __DIR__ . '/vendor/guzzlehttp/psr7/src/functions.php';
 require_once __DIR__ . '/vendor/guzzlehttp/psr7/src/Stream.php';
 require_once __DIR__ . '/vendor/guzzlehttp/psr7/src/MessageTrait.php';
+require_once __DIR__ . '/vendor/guzzlehttp/psr7/src/Query.php';
 require_once __DIR__ . '/vendor/guzzlehttp/psr7/src/Request.php';
 require_once __DIR__ . '/vendor/guzzlehttp/psr7/src/Uri.php';
 require_once __DIR__ . '/vendor/guzzlehttp/psr7/src/UriResolver.php';
+require_once __DIR__ . '/vendor/guzzlehttp/psr7/src/Utils.php';
 require_once __DIR__ . '/vendor/guzzlehttp/psr7/src/Response.php';
 
 require_once __DIR__ . '/vendor/guzzlehttp/promises/src/functions.php';
+require_once __DIR__ . '/vendor/guzzlehttp/promises/src/Create.php';
+require_once __DIR__ . '/vendor/guzzlehttp/promises/src/Is.php';
 require_once __DIR__ . '/vendor/guzzlehttp/promises/src/PromiseInterface.php';
 require_once __DIR__ . '/vendor/guzzlehttp/promises/src/Promise.php';
 require_once __DIR__ . '/vendor/guzzlehttp/promises/src/TaskQueueInterface.php';
 require_once __DIR__ . '/vendor/guzzlehttp/promises/src/TaskQueue.php';
 require_once __DIR__ . '/vendor/guzzlehttp/promises/src/FulfilledPromise.php';
 require_once __DIR__ . '/vendor/guzzlehttp/promises/src/RejectedPromise.php';
+require_once __DIR__ . '/vendor/guzzlehttp/promises/src/Utils.php';
 
 require_once __DIR__ . '/vendor/monolog/monolog/src/Monolog/ResettableInterface.php';
 require_once __DIR__ . '/vendor/monolog/monolog/src/Monolog/Logger.php';
