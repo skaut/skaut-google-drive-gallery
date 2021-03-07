5409b7ba24732fbd35757d629db7ef05f4d6d598
Marek Dědič <developer@dedic.eu>
2021-03-07 18:07:15 +0100

    Parameterized prefix

--- scoper.inc.php
+++ scoper.inc.php
@@ -11,20 +11,22 @@ return array(
 	),
 	'patchers'                   => array(
 		function ( $file_path, $prefix, $contents ) {
+			$regex_prefix = mb_ereg_replace( '\\\\', '\\\\\\\\', $prefix );
+			$prefix = mb_ereg_replace( '\\\\', '\\\\', $prefix );
 			if ( __DIR__ . '/dist/bundled/vendor/symfony/polyfill-mbstring/Mbstring.php' === $file_path ) {
 				return $contents; // TODO: Remove after nikic/PHP-Parser#763 is solved.
 			}
 			if ( in_array( $file_path, array( __DIR__ . '/dist/bundled/vendor/symfony/polyfill-intl-idn/bootstrap.php', __DIR__ . '/dist/bundled/vendor/symfony/polyfill-mbstring/bootstrap.php' ), true ) ) {
-				$contents = mb_ereg_replace( 'namespace Sgdg\\\\Vendor;', '', $contents );
+				$contents = mb_ereg_replace( "namespace {$prefix};", '', $contents );
 			}
 			if ( __DIR__ . '/dist/bundled/vendor/guzzlehttp/guzzle/src/functions.php' === $file_path ) {
-				$contents = mb_ereg_replace( "\\\\Sgdg\\\\Vendor\\\\uri_template\(", "\\uri_template(", $contents );
+				$contents = mb_ereg_replace( "\\\\{$prefix}\\\\uri_template\(", "\\uri_template(", $contents );
 			}
 			if ( __DIR__ . '/dist/bundled/vendor/google/apiclient/src/aliases.php' === $file_path ) {
-				$contents = mb_ereg_replace( "'Sgdg\\\\\\\\Vendor\\\\\\\\Google\\\\\\\\(.*?)'\\s+=> 'Google_(.*?)'", "'Sgdg\\\\Vendor\\\\Google\\\\\\1' => 'Sgdg\\\\Vendor\\\\Google_\\2'", $contents );
+				$contents = mb_ereg_replace( "'{$regex_prefix}\\\\\\\\Google\\\\\\\\(.*?)'\\s+=> 'Google_(.*?)'", "'{$prefix}\\\\Google\\\\\\1' => '{$prefix}\\\\Google_\\2'", $contents );
 			}
-			$contents = mb_ereg_replace( "defined\('(\\\\\\\\)?GuzzleHttp", "defined('\\\\Sgdg\\\\Vendor\\\\GuzzleHttp", $contents );
-			$contents = mb_ereg_replace( "array\('Monolog\\\\\\\\Utils', 'detectAndCleanUtf8'\)", "array('\\\\Sgdg\\\\Vendor\\\\Monolog\\\\Utils', 'detectAndCleanUtf8')", $contents );
+			$contents = mb_ereg_replace( "defined\('(\\\\\\\\)?GuzzleHttp", "defined('\\\\{$prefix}\\\\GuzzleHttp", $contents );
+			$contents = mb_ereg_replace( "array\('Monolog\\\\\\\\Utils', 'detectAndCleanUtf8'\)", "array('\\\\{$prefix}\\\\Monolog\\\\Utils', 'detectAndCleanUtf8')", $contents );
 
 			return $contents;
 		},
