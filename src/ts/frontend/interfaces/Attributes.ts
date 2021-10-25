974f3bc9b9bfcc603f4f537f323e2a8831fc0877
Marek Dědič <developer@dedic.eu>
2021-10-25 11:40:38 +0200

    consistent-indexed-object-style

--- src/ts/frontend/interfaces/Attributes.ts
+++ src/ts/frontend/interfaces/Attributes.ts
@@ -1,5 +1,6 @@
 /* exported Attributes */
 
-interface Attributes {
-	[ index: string ]: number | string | Array< string > | undefined;
-}
+type Attributes = Record<
+	string,
+	number | string | Array< string > | undefined
+>;
