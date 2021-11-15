95ba60a4aafa6f4fdd24b881262b38e8709b19c6
Marek Dědič <developer@dedic.eu>
2021-11-16 00:49:06 +0100

    Lint fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,7 +15,7 @@ if ( PHP_MAJOR_VERSION >= 8 ) {
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-if ( ! $_tests_dir ) {
+if ( '' === $_tests_dir || false === $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
@@ -31,11 +31,11 @@ require_once "{$_tests_dir}/includes/functions.php";
 /**
  * Manually load the plugin being tested.
  */
-function _manually_load_plugin() {
+$_manually_load_plugin = static function() {
 	require dirname( dirname( __FILE__ ) ) . '/dist/skaut-google-drive-gallery.php';
-}
+};
 
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+tests_add_filter( 'muplugins_loaded', $_manually_load_plugin );
 
 // Start up the WP testing environment.
 require "{$_tests_dir}/includes/bootstrap.php";
