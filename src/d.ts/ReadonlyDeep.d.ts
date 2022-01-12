ff66b149c8d78312eb93f67fefe5712e223373dc
Marek Dědič <developer@dedic.eu>
2022-01-12 22:19:45 +0100

    Updated ReadonlyDeep

--- src/d.ts/ReadonlyDeep.d.ts
+++ src/d.ts/ReadonlyDeep.d.ts
@@ -1,3 +1,8 @@
+/**
+Matches any [primitive value](https://developer.mozilla.org/en-US/docs/Glossary/Primitive).
+
+@category Type
+*/
 declare type Primitive =
 	| null
 	| undefined
@@ -9,25 +14,37 @@ declare type Primitive =
 
 /**
 Convert `object`s, `Map`s, `Set`s, and `Array`s and all of their keys/elements into immutable structures recursively.
+
 This is useful when a deeply nested structure needs to be exposed as completely immutable, for example, an imported JSON module or when receiving an API response that is passed around.
+
 Please upvote [this issue](https://github.com/microsoft/TypeScript/issues/13923) if you want to have this type as a built-in in TypeScript.
+
 @example
 ```
 // data.json
 {
 	"foo": ["bar"]
 }
+
 // main.ts
 import {ReadonlyDeep} from 'type-fest';
 import dataJson = require('./data.json');
+
 const data: ReadonlyDeep<typeof dataJson> = dataJson;
+
 export default data;
+
 // test.ts
 import data from './main';
+
 data.foo.push('bar');
 //=> error TS2339: Property 'push' does not exist on type 'readonly string[]'
 ```
-@category Utilities
+
+@category Object
+@category Array
+@category Set
+@category Map
 */
 declare type ReadonlyDeep<T> = T extends Primitive | ((...arguments: any[]) => unknown)
 	? T
@@ -42,18 +59,18 @@ declare type ReadonlyDeep<T> = T extends Primitive | ((...arguments: any[]) => u
 /**
 Same as `ReadonlyDeep`, but accepts only `ReadonlyMap`s as inputs. Internal helper for `ReadonlyDeep`.
 */
-declare interface ReadonlyMapDeep<KeyType, ValueType>
+interface ReadonlyMapDeep<KeyType, ValueType>
 	extends ReadonlyMap<ReadonlyDeep<KeyType>, ReadonlyDeep<ValueType>> {}
 
 /**
 Same as `ReadonlyDeep`, but accepts only `ReadonlySet`s as inputs. Internal helper for `ReadonlyDeep`.
 */
-declare interface ReadonlySetDeep<ItemType>
+interface ReadonlySetDeep<ItemType>
 	extends ReadonlySet<ReadonlyDeep<ItemType>> {}
 
 /**
 Same as `ReadonlyDeep`, but accepts only `object`s as inputs. Internal helper for `ReadonlyDeep`.
 */
-declare type ReadonlyObjectDeep<ObjectType extends object> = {
+type ReadonlyObjectDeep<ObjectType extends object> = {
 	readonly [KeyType in keyof ObjectType]: ReadonlyDeep<ObjectType[KeyType]>
 };
