242ad1dce7ad31f72c655f6fa5acdeefa0062705
Marek Dědič <developer@dedic.eu>
2022-04-02 18:45:55 +0200

    Added missing Helpers file

---
+++ src/php/helpers/class-helpers.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Contains the Helpers class.
+ *
+ * @package skaut-google-drive-gallery
+ */
+
+namespace Sgdg;
+
+/**
+ * Contains various helper functions.
+ */
+class Helpers {
+	/**
+	 * Checks whether debug info should be displayed
+	 *
+	 * @return bool True to display debug info.
+	 */
+	public static function is_debug_display() {
+		if ( defined( 'WP_DEBUG' ) && defined( 'WP_DEBUG_DISPLAY' ) ) {
+			return \WP_DEBUG === true && \WP_DEBUG_DISPLAY === true;
+		}
+		return false;
+	}
+
+	/**
+	 * Runs an AJAX handler and handles errors.
+	 *
+	 * @param callable $handler The actual handler.
+	 *
+	 * @return void
+	 */
+	public static function ajax_wrapper($handler) {
+		try {
+			$handler();
+		} catch ( \Sgdg\Exceptions\Exception $e ) {
+			wp_send_json( array( 'error' => $e->getMessage() ) );
+		} catch ( \Exception $e ) {
+			if ( \Sgdg\Helpers::is_debug_display() ) {
+				wp_send_json( array( 'error' => $e->getMessage() ) );
+			}
+			wp_send_json( array( 'error' => esc_html__( 'Unknown error.', 'skaut-google-drive-gallery' ) ) );
+		}
+	}
+}
