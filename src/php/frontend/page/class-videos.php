80012e1af3af4593ce67e840504a49390391bee6
Marek Dědič <developer@dedic.eu>
2022-07-20 11:34:09 +0200

    Filtering out videos without a thumbnail

--- src/php/frontend/page/class-videos.php
+++ src/php/frontend/page/class-videos.php
@@ -40,6 +40,12 @@ class Videos {
 			$pagination_helper
 		)->then(
 			static function( $raw_videos ) use ( &$options ) {
+				$raw_videos = array_values( array_filter(
+					$raw_videos,
+					static function( $video ) {
+						return ! is_null( $video['thumbnailLink'] );
+					}
+				) );
 				$videos             = array_map(
 					static function( $video ) use ( &$options ) {
 						return array(
