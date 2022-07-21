67c820ec20a966c57451039a8b456f65c04b28e3
Marek Dědič <developer@dedic.eu>
2022-07-21 11:23:26 +0200

    Removed obsolete Infinite_Pagination_Helper

--- src/php/skaut-google-drive-gallery.php
+++ src/php/skaut-google-drive-gallery.php
@@ -82,7 +82,6 @@ require_once __DIR__ . '/frontend/class-api-fields.php';
 require_once __DIR__ . '/frontend/class-block.php';
 require_once __DIR__ . '/frontend/class-gallery.php';
 require_once __DIR__ . '/frontend/class-gallery-context.php';
-require_once __DIR__ . '/frontend/class-infinite-pagination-helper.php';
 require_once __DIR__ . '/frontend/class-pagination-helper.php';
 require_once __DIR__ . '/frontend/class-options-proxy.php';
 require_once __DIR__ . '/frontend/class-page.php';
