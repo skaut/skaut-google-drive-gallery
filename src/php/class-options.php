7f4d256c44d455913fbb6fe6391168d9bbd4d3d2
Marek Dědič <developer@dedic.eu>
2022-07-22 11:54:57 +0200

    Code reorg

--- src/php/class-options.php
+++ src/php/class-options.php
@@ -22,11 +22,10 @@ require_once __DIR__ . '/admin/class-readonly-string-option.php';
  *
  * Contains all the options for the plugin as static properties.
  *
- * phpcs:disable SlevomatCodingStandard.Classes.ForbiddenPublicProperty.ForbiddenPublicProperty
- *
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  * @SuppressWarnings(PHPMD.LongVariable)
  * @SuppressWarnings(PHPMD.TooManyFields)
+ * phpcs:disable SlevomatCodingStandard.Classes.ForbiddenPublicProperty.ForbiddenPublicProperty
  */
 final class Options {
 
