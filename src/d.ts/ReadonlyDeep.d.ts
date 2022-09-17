a5025cb56748c75deeb2885e2328808a7154e302
Marek Dědič <developer@dedic.eu>
2022-09-17 18:00:02 +0200

    Updated ReadonlyDeep

--- src/d.ts/ReadonlyDeep.d.ts
+++ src/d.ts/ReadonlyDeep.d.ts
@@ -1,9 +1,8 @@
 /**
 Matches any [primitive value](https://developer.mozilla.org/en-US/docs/Glossary/Primitive).
-
 @category Type
 */
-declare type Primitive =
+type Primitive =
 	| null
 	| undefined
 	| string
@@ -11,6 +10,8 @@ declare type Primitive =
 	| boolean
 	| symbol
 	| bigint;
+
+type BuiltIns = Primitive | Date | RegExp;
 
 /**
 Convert `object`s, `Map`s, `Set`s, and `Array`s and all of their keys/elements into immutable structures recursively.
@@ -27,7 +28,7 @@ Please upvote [this issue](https://github.com/microsoft/TypeScript/issues/13923)
 }
 
 // main.ts
-import {ReadonlyDeep} from 'type-fest';
+import type {ReadonlyDeep} from 'type-fest';
 import dataJson = require('./data.json');
 
 const data: ReadonlyDeep<typeof dataJson> = dataJson;
@@ -46,27 +47,31 @@ data.foo.push('bar');
 @category Set
 @category Map
 */
-declare type ReadonlyDeep<T> = T extends Primitive | ((...arguments: any[]) => unknown)
+declare type ReadonlyDeep<T> = T extends BuiltIns
 	? T
-	: T extends ReadonlyMap<infer KeyType, infer ValueType>
-	? ReadonlyMapDeep<KeyType, ValueType>
-	: T extends ReadonlySet<infer ItemType>
-	? ReadonlySetDeep<ItemType>
-	: T extends object
-	? ReadonlyObjectDeep<T>
-	: unknown;
+	: T extends (...arguments: any[]) => unknown
+		? {} extends ReadonlyObjectDeep<T>
+			? T
+			: HasMultipleCallSignatures<T> extends true
+				? T
+				: ((...arguments: Parameters<T>) => ReturnType<T>) & ReadonlyObjectDeep<T>
+		: T extends Readonly<ReadonlyMap<infer KeyType, infer ValueType>>
+			? ReadonlyMapDeep<KeyType, ValueType>
+			: T extends Readonly<ReadonlySet<infer ItemType>>
+				? ReadonlySetDeep<ItemType>
+				: T extends object
+					? ReadonlyObjectDeep<T>
+					: unknown;
 
 /**
 Same as `ReadonlyDeep`, but accepts only `ReadonlyMap`s as inputs. Internal helper for `ReadonlyDeep`.
 */
-interface ReadonlyMapDeep<KeyType, ValueType>
-	extends ReadonlyMap<ReadonlyDeep<KeyType>, ReadonlyDeep<ValueType>> {}
+type ReadonlyMapDeep<KeyType, ValueType> = {} & Readonly<ReadonlyMap<ReadonlyDeep<KeyType>, ReadonlyDeep<ValueType>>>;
 
 /**
 Same as `ReadonlyDeep`, but accepts only `ReadonlySet`s as inputs. Internal helper for `ReadonlyDeep`.
 */
-interface ReadonlySetDeep<ItemType>
-	extends ReadonlySet<ReadonlyDeep<ItemType>> {}
+type ReadonlySetDeep<ItemType> = {} & Readonly<ReadonlySet<ReadonlyDeep<ItemType>>>;
 
 /**
 Same as `ReadonlyDeep`, but accepts only `object`s as inputs. Internal helper for `ReadonlyDeep`.
@@ -74,3 +79,18 @@ Same as `ReadonlyDeep`, but accepts only `object`s as inputs. Internal helper fo
 type ReadonlyObjectDeep<ObjectType extends object> = {
 	readonly [KeyType in keyof ObjectType]: ReadonlyDeep<ObjectType[KeyType]>
 };
+
+/**
+Test if the given function has multiple call signatures.
+
+Needed to handle the case of a single call signature with properties.
+
+Multiple call signatures cannot currently be supported due to a TypeScript limitation.
+@see https://github.com/microsoft/TypeScript/issues/29732
+*/
+type HasMultipleCallSignatures<T extends (...arguments: any[]) => unknown> =
+	T extends {(...arguments: infer A): unknown; (...arguments: any[]): unknown}
+		? unknown[] extends A
+			? false
+			: true
+		: false;
