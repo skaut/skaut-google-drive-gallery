229a505ec0a4b2a030d9634ebdecaac53308906c
Marek Dědič <developer@dedic.eu>
2024-09-14 12:52:54 +0300

    No non-null assertions

--- src/ts/admin/tinymce.ts
+++ src/ts/admin/tinymce.ts
@@ -9,7 +9,7 @@ function tinymceSubmit(): void {
 	if ($('#sgdg-tinymce-insert').attr('disabled') !== undefined) {
 		return;
 	}
-	tinymce.activeEditor!.insertContent('[sgdg path="' + path.join('/') + '"]');
+	tinymce.activeEditor?.insertContent('[sgdg path="' + path.join('/') + '"]');
 	tb_remove();
 }
 
