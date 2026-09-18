b8c9494d0e0e71f724395d8ad96fb84a3589e6fd
Marek Dědič <developer@dedic.eu>
2026-09-18 17:08:01 +0200

    Moved react children from props

--- src/ts/frontend/block/SgdgSettingsOverrideComponent.ts
+++ src/ts/frontend/block/SgdgSettingsOverrideComponent.ts
@@ -14,53 +14,50 @@ interface SgdgSettingsOverrideComponentProps {
 export class SgdgSettingsOverrideComponent extends Component<SgdgSettingsOverrideComponentProps> {
 	public override render(): React.ReactNode {
 		const { editor } = this.props;
-		return createElement(PanelBody, {
-			children: [
-				createElement('h3', null, sgdgBlockLocalize.grid_section_name),
-				createElement(SgdgIntegerSettingsComponent, {
-					editor,
-					name: 'grid_height',
-				}),
-				createElement(SgdgIntegerSettingsComponent, {
-					editor,
-					name: 'grid_spacing',
-				}),
-				createElement(SgdgBooleanSettingsComponent, {
-					editor,
-					name: 'dir_counts',
-				}),
-				createElement(SgdgIntegerSettingsComponent, {
-					editor,
-					name: 'page_size',
-				}),
-				createElement(SgdgBooleanSettingsComponent, {
-					editor,
-					name: 'page_autoload',
-				}),
-				createElement(SgdgOrderingSettingsComponent, {
-					editor,
-					name: 'image_ordering',
-				}),
-				createElement(SgdgOrderingSettingsComponent, {
-					editor,
-					name: 'dir_ordering',
-				}),
-				createElement(
-					'h3',
-					null,
-					sgdgBlockLocalize.lightbox_section_name
-				),
-				createElement(SgdgIntegerSettingsComponent, {
-					editor,
-					name: 'preview_size',
-				}),
-				createElement(SgdgBooleanSettingsComponent, {
-					editor,
-					name: 'preview_loop',
-				}),
-			],
-			className: 'sgdg-block-settings',
-			title: sgdgBlockLocalize.settings_override,
-		});
+		return createElement(
+			PanelBody,
+			{
+				className: 'sgdg-block-settings',
+				title: sgdgBlockLocalize.settings_override,
+			},
+			createElement('h3', null, sgdgBlockLocalize.grid_section_name),
+			createElement(SgdgIntegerSettingsComponent, {
+				editor,
+				name: 'grid_height',
+			}),
+			createElement(SgdgIntegerSettingsComponent, {
+				editor,
+				name: 'grid_spacing',
+			}),
+			createElement(SgdgBooleanSettingsComponent, {
+				editor,
+				name: 'dir_counts',
+			}),
+			createElement(SgdgIntegerSettingsComponent, {
+				editor,
+				name: 'page_size',
+			}),
+			createElement(SgdgBooleanSettingsComponent, {
+				editor,
+				name: 'page_autoload',
+			}),
+			createElement(SgdgOrderingSettingsComponent, {
+				editor,
+				name: 'image_ordering',
+			}),
+			createElement(SgdgOrderingSettingsComponent, {
+				editor,
+				name: 'dir_ordering',
+			}),
+			createElement('h3', null, sgdgBlockLocalize.lightbox_section_name),
+			createElement(SgdgIntegerSettingsComponent, {
+				editor,
+				name: 'preview_size',
+			}),
+			createElement(SgdgBooleanSettingsComponent, {
+				editor,
+				name: 'preview_loop',
+			})
+		);
 	}
 }
